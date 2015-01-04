a6582457c01c535cbff9712fa8faa055148ac192
@JuanBalbuena4 <hola@juanbalbuena.es>
2015-01-04 19:36:13 +0100

    version 1.0
    final commit version 1.0

--- js/form_validate.js
+++ js/form_validate.js
@@ -1,64 +1,88 @@
 $(document).ready(function(){
+
+	var message = $("#message"); var email = $("#email"); var name = $("#name");
+
 	$('form').submit(function() {
 		$('input[type=submit]').attr('disabled', 'true');
 	});
+
 	$('form').submit(function() {
 		var boton = $(this).find(':submit');
 		boton.attr('disabled', 'true');
 		boton.val('process');
 	});
-	$("#message").on("focusout", function(){
+
+	message.on("focusout", function(){
 		if($(this).val() == ""){
-			$("#message").css({"background": "#FFCECF",
-				"border": "2px solid #FF726C"});
-			$(".ok1").remove();
-			$("#message").focusout($(".cancel1").remove());
-			$("#message").after('<img class="cancel1" src="img/cancel.png" width="28" height="28">');
-			$("#message").attr("placeholder", "You must write a message");
+			message.css({"background": "#FFCECF",
+								"border": "2px solid #FF726C"});
+			for(var i = 0; i < message.length; i++){
+				$(".ok1").remove();
+				$(".cancel1").remove();
+				message.after('<img class="cancel1" src="img/cancel.png" width="28" height="28">');
+				message.attr("placeholder", "You must write a message");
+			}
 		}
 		else{
-			$("#message").css({"background": "#C8FDD4",
+			message.css({"background": "#C8FDD4",
 				"border": "2px solid #52FD8B"});
-			$('.cancel1').remove();
-			$("#message").focusout($(".ok1").remove());
-			$("#message").after('<img class="ok1" src="img/check.png" width="28" height="28">');
+
+			for(var i = 0; i < message.length; i++){
+				$(".cancel1").remove();
+				$(".ok1").remove();
+				message.after('<img class="ok1" src="img/check.png" width="28" height="28">');
+			}
 		}
 	});
-	$("#email").on("focusout", function(){
+	email.on("focusout", function(){
 		if($(this).val() == "" || $("#email").val().indexOf("@") == -1 || $("#email").val().indexOf(".") == -1){
-			$("#email").css({"background": "#FFCECF",
+			email.css({"background": "#FFCECF",
 				"border": "2px solid #FF726C"});
-			$(".ok2").remove();
-			$("#email").focusout($(".cancel2").remove());
-			$("#email").after('<img class="cancel2" src="img/cancel.png" width="28" height="28">');
-			$("#email").attr("placeholder", "You must write your email");
+
+			for(var i = 0; i < email.length; i++){
+				$(".ok2").remove();
+				$(".cancel2").remove();
+				email.after('<img class="cancel2" src="img/cancel.png" width="28" height="28">');
+				email.attr("placeholder", "You must write your email");
+			}
+
 		}
 		else{
-			$("#email").css({"background": "#C8FDD4",
+			email.css({"background": "#C8FDD4",
 				"border": "2px solid #52FD8B"});
-			$(".cancel2").remove();
-			$("#email").focusout($(".ok2").remove());
-			$("#email").after('<img class="ok2" src="img/check.png" width="28" height="28">');
+
+			for(var i = 0; i < email.length; i++){
+				$(".cancel2").remove();
+				$(".ok2").remove();
+				email.after('<img class="ok2" src="img/check.png" width="28" height="28">');
+			}
 		}
 	});
-	$("#name").on("focusout", function(){
+	name.on("focusout", function(){
 		if($(this).val() == ""){
-			$("#name").css({"background": "#FFCECF",
+			name.css({"background": "#FFCECF",
 				"border": "2px solid #FF726C"});
-			$(".ok3").remove();
-			$("#name").focusout($(".cancel3").remove());
-			$("#name").after('<img class="cancel3" src="img/cancel.png" width="28" height="28">');
-			$("#name").attr("placeholder", "You must write your name");
+
+			for( var i = 0; i < name.length; i++){
+				$(".ok3").remove();
+				$(".cancel3").remove();
+				name.after('<img class="cancel3" src="img/cancel.png" width="28" height="28">');
+				name.attr("placeholder", "You must write your name");
+			}
 		}
 		else{
-			$("#name").css({"background": "#C8FDD4",
+			name.css({"background": "#C8FDD4",
 				"border": "2px solid #52FD8B"});
-			$(".cancel3").remove();
-			$("#name").focusout($(".ok3").remove());
-			$("#name").after('<img class="ok3" src="img/check.png" width="28" height="28">');
+
+			for(var i = 0; i < name.lenght; i++){
+				$(".cancel3").remove();
+				$(".ok3").remove();
+				name.after('<img class="ok3" src="img/check.png" width="28" height="28">');
+			}
 		}
 	});
+
 	$("#range").on("change", function(){
 		$("#count").html(this.value + "€");
 	});
-})
+});
