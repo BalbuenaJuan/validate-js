9a666a7d0d9fabada620e463d863441b9e002054
@JuanBalbuena4 <hola@juanbalbuena.es>
2015-02-08 22:42:22 +0100

    V1
    V1

--- js/form_validate.js
+++ js/form_validate.js
@@ -12,82 +12,91 @@ $(document).ready(function(){
 		boton.val('process');
 	});
 
-	message.on("focusout", function(){
-		if($(this).val() == ""){
+    message.on("focusout", function(){
+        if($(this).val() == ""){
 
-			for(var i = 0; i < message.length; i++){
-				message.css({"background": "#FFCECF",
-					"border": "2px solid #FF726C"});
-				$(".ok1").remove();
-				$(".cancel1").remove();
-				message.after('<img class="cancel1" src="img/cancel.png" width="28" height="28">');
-				message.attr("placeholder", "You must write a message");
-                $("#message-fail").show();
-			}
-		}
-		else{
+            for(var i = 0; i < message.length; i++){
+                message.css({"background": "#FFCECF",
+                    "border": "2px solid #FF726C"});
+                $("#ok1").remove();
+                $("#cancel1").remove();
+                $("#bad-message").remove();
+                message.after('<img id="cancel1" src="img/cancel.png" width="28" height="28">');
+                message.attr("placeholder", "You must write a message");
+                message.before('<p id="bad-message">! Bad message</p>');
+                $("#bad-message").css({"color": "red",
+                    "margin-bottom": "0"});
+            }
+        }
+        else{
+            for(var i = 0; i < message.length; i++){
+                message.css({"background": "#C8FDD4",
+                    "border": "2px solid #52FD8B"});
+                $("#cancel1").remove();
+                $("#ok1").remove();
+                message.after('<img id="ok1" src="img/check.png" width="28" height="28">');
+                $("#bad-message").remove();
 
-			for(var i = 0; i < message.length; i++){
-				message.css({"background": "#C8FDD4",
-					"border": "2px solid #52FD8B"});
-				$(".cancel1").remove();
-				$(".ok1").remove();
-				message.after('<img class="ok1" src="img/check.png" width="28" height="28">');
-				$("#message-fail").fadeOut();
-			}
-		}
-	});
-	email.on("focusout", function(){
-		if($(this).val() == "" || $("#email").val().indexOf("@") == -1 || $("#email").val().indexOf(".") == -1){
+            }
+        }
+    });
+    email.on("focusout", function(){
+        if($(this).val() == "" || $("#email").val().indexOf("@") == -1 || $("#email").val().indexOf(".") == -1){
 
-			for(var i = 0; i < email.length; i++){
-				email.css({"background": "#FFCECF",
-					"border": "2px solid #FF726C"});
-				$(".ok2").remove();
-				$(".cancel2").remove();
-				email.after('<img class="cancel2" src="img/cancel.png" width="28" height="28">');
-				email.attr("placeholder", "You must write your email");
-                $("#email-fail").show();
-			}
+            for(var i = 0; i < email.length; i++){
+                email.css({"background": "#FFCECF",
+                    "border": "2px solid #FF726C"});
+                $("#ok2").remove();
+                $("#cancel2").remove();
+                $("#bad-email").remove();
+                email.after('<img id="cancel2" src="img/cancel.png" width="28" height="28">');
+                email.attr("placeholder", "You must write a email");
+                email.before('<p id="bad-email">! Bad email</p>');
+                $("#bad-email").css({"color": "red",
+                    "margin-bottom": "0"});
+            }
 
-		}
-		else{
+        }
+        else{
+            for(var i = 0; i < email.length; i++){
+                email.css({"background": "#C8FDD4",
+                    "border": "2px solid #52FD8B"});
+                $("#cancel2").remove();
+                $("#ok2").remove();
+                $("#bad-email").remove();
+                email.after('<img id="ok2" src="img/check.png" width="28" height="28">');
+                $("#bad-email").remove();
+            }
+        }
+    });
+    name.on("focusout", function(){
+        if($(this).val() == ""){
 
-			for(var i = 0; i < email.length; i++){
-				email.css({"background": "#C8FDD4",
-					"border": "2px solid #52FD8B"});
-				$(".cancel2").remove();
-				$(".ok2").remove();
-				email.after('<img class="ok2" src="img/check.png" width="28" height="28">');
-				$("#email-fail").fadeOut();
-			}
-		}
-	});
-	name.on("focusout", function(){
-		if($(this).val() == ""){
+            for( var i = 0; i < name.length; i++){
+                name.css({"background": "#FFCECF",
+                    "border": "2px solid #FF726C"});
+                $("#ok3").remove();
+                $("#cancel3").remove();
+                $("#bad-name").remove();
+                name.after('<img id="cancel3" src="img/cancel.png" width="28" height="28">');
+                name.attr("placeholder", "You must write a name");
+                name.before('<p id="bad-name">! Bad name </p>');
+                $("#bad-name").css({"color": "red",
+                    "margin-bottom": "0"});
 
-			for( var i = 0; i < name.length; i++){
-				name.css({"background": "#FFCECF",
-					"border": "2px solid #FF726C"});
-				$(".ok3").remove();
-				$(".cancel3").remove();
-				name.after('<img class="cancel3" src="img/cancel.png" width="28" height="28">');
-				name.attr("placeholder", "You must write your name");
-                $("#text-fail").show();
-			}
-		}
-		else{
-
-			for(var i = 0; i < name.length; i++){
-				name.css({"background": "#C8FDD4",
-					"border": "2px solid #52FD8B"});
-				$(".cancel3").remove();
-				$(".ok3").remove();
-				name.after('<img class="ok3" src="img/check.png" width="28" height="28">');
-				$("#text-fail").fadeOut();
-			}
-		}
-	});
+            }
+        }
+        else{
+            for(var i = 0; i < name.length; i++){
+                name.css({"background": "#C8FDD4",
+                    "border": "2px solid #52FD8B"});
+                $("#cancel3").remove();
+                $("#ok3").remove();
+                name.after('<img id="ok3" src="img/check.png" width="28" height="28">');
+                $("#bad-name").remove();
+            }
+        }
+    });
 
 	$("#range").on("change", function(){
 		$("#count").html(this.value + "€");
